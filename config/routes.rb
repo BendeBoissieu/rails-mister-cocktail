2a1bb4a14d75600215b9da33195bced0900f51cf
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-07 15:17:19 +0200

    cocktails controller without doses and ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'cocktails/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doses, :cocktails
+  root to: 'cocktails#index'
+
 end
